6c1faef0e9c6895bc0e977cffb95abef4d3e4312
besnovatyj <metr31@yandex.ru>
2026-06-15 19:55:51 +0500

    modmanNew -> modman

--- src/Module.php
+++ src/Module.php
@@ -9,9 +9,9 @@ declare(strict_types=1);
 namespace Besnovatyj\Config;
 
 use common\components\module\CmsModule;
-use modules\modmanNew\contract\DeclaresModule;
-use modules\modmanNew\contract\ProvidesAdminMenu;
-use modules\modmanNew\contract\ProvidesOptions;
+use modules\modman\contract\DeclaresModule;
+use modules\modman\contract\ProvidesAdminMenu;
+use modules\modman\contract\ProvidesOptions;
 
 /**
  * Config Module - управление динамической конфигурацией приложения
