e07d30ee0ecccf0c0816da49f29744ede8050d32
besnovatyj <metr31@yandex.ru>
2026-07-02 21:52:12 +0500

    extract modules contracts

--- src/Module.php
+++ src/Module.php
@@ -9,9 +9,9 @@ declare(strict_types=1);
 namespace Besnovatyj\Config;
 
 use common\components\module\CmsModule;
-use modules\modman\contract\DeclaresModule;
-use modules\modman\contract\ProvidesAdminMenu;
-use modules\modman\contract\ProvidesOptions;
+use Besnovatyj\Contracts\module\DeclaresModule;
+use Besnovatyj\Contracts\module\ProvidesAdminMenu;
+use Besnovatyj\Contracts\module\ProvidesOptions;
 
 /**
  * Config Module - управление динамической конфигурацией приложения
